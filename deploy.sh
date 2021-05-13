be49664c1723832f8bee94d7c32f5ef9e35a2089
suo <59600018+suodrazah@users.noreply.github.com>
2021-05-13 18:56:09 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -71,8 +71,8 @@ if [ $PROCEED != "Y" ]; then
 fi
 read -p "Admin email? (for certbot/https): " ADMIN_EMAIL
 read -p "Domain? e.g. example.com: " DOMAIN
-read -p "Desired Traefik dashboard username?: " USERNAME
-echo Desired Traefik dashboard password?:
+read -p "Traefik dashboard username?: " USERNAME
+echo Traefik dashboard password?
 #Prepare traefik password
 export "HASHED_PASSWORD=$(openssl passwd -apr1 $PASSWORD)"
 read -p "Timezone? (Australia/Hobart): " TIMEZONE
