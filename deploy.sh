62c8b180d29635a9d8d6289f8f27fcb2cc5aeb9f
suo <59600018+suodrazah@users.noreply.github.com>
2021-05-13 18:44:50 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,69 @@
 
 #This script is a work in development, I mean...look at it, if it was a beloved family pet I'd put it down.
 
+
+
+#Update
+read -p "Update or NEW: (u/N) " UPDATE
+if [ $UPDATE != "u" ]; then
+    echo "Updating!"
+    . /home/$USER/deploy.conf
+    #Update
+    sudo /usr/bin/apt-get update && sudo /usr/bin/apt-get full-upgrade -y
+    export HOSTNAME="primary.$DOMAIN"
+    echo $HOSTNAME | sudo tee /etc/hostname > /dev/null 2>&1
+    sudo hostname -F /etc/hostname
+    sudo timedatectl set-timezone $TIMEZONE
+    ##Install Docker
+    curl -fsSL get.docker.com -o get-docker.sh
+    CHANNEL=stable sh get-docker.sh
+    rm get-docker.sh
+    #Initiate Docker Swarm
+    docker swarm init
+    
+    #Create traefik overlay networks
+    docker network create --driver=overlay traefik-public
+    
+    #Add label to this node so we can constrain traefik and portainer to it
+    export NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
+    docker node update --label-add primary=true $NODE_ID
+    
+    #Deploy traefik and portainer
+    curl -L https://raw.githubusercontent.com/suodrazah/docker_swarm/main/deploy/traefik.yml -o traefik.yml
+    curl -L https://raw.githubusercontent.com/suodrazah/docker_swarm/main/deploy/portainer.yml -o portainer.yml
+    export DOMAIN=$DOMAIN
+    export HASHED_PASSWORD=$HASHED_PASSWORD
+    export EMAIL=$EMAIL
+    export USERNAME=$USERNAME
+    docker stack deploy -c traefik.yml traefik
+    docker stack deploy -c portainer.yml portainer
+    docker swarm update --task-history-limit=1
+    clear
+    echo "Deployment (probably) complete... please visit https://traefik."$DOMAIN" and https://portainer."$DOMAIN
+    echo "Exiting in a few seconds"
+    sleep 10
+    
+    #Clean up
+    rm /home/$USER/deploy.sh
+    rm /home/$USER/traefik.yml
+    rm /home/$USER/portainer.yml
+    clear
+    
+    #Done
+    sleep 1
+    exit
+fi
+
+rm ./deploy.conf
+clear
 #Set variables
 read -p "Does this server have a domain that points at it, and ports 80 and 443 exposed? (Y/n): " PROCEED
 PROCEED=${PROCEED:-Y}
 if [ $PROCEED != "Y" ]; then
     echo "Then this won't work...goodbye"
-        rm /home/$USER/deploy.sh
-        sleep 5
-	exit
+    rm /home/$USER/deploy.sh
+    sleep 5
+    exit
 fi
 read -p "Admin email? (for certbot/https): " ADMIN_EMAIL
 read -p "Domain? e.g. example.com: " DOMAIN
@@ -29,16 +84,16 @@ echo Timezone - $TIMEZONE
 read -p "Is this correct? (Y/n): " PROCEED
 PROCEED=${PROCEED:-Y}
 if [ $PROCEED != "Y" ]; then
-        rm /home/$USER/deploy.sh
-        sleep 5
-	exit
+    rm /home/$USER/deploy.sh
+    sleep 5
+    exit
 fi
 
-echo "$DOMAIN" >> deploy.conf
-echo "$ADMIN_EMAIL" >> deploy.conf
-echo "$USERNAME" >> deploy.conf
-echo "$HASHED_PASSWORD" >> deploy.conf
-echo "$TIMEZONE" >> deploy.conf
+echo "DOMAIN=$DOMAIN" >> deploy.conf
+echo "ADMIN_EMAIL=$ADMIN_EMAIL" >> deploy.conf
+echo "USERNAME=$USERNAME" >> deploy.conf
+echo "HASHED_PASSWORD=$HASHED_PASSWORD" >> deploy.conf
+echo "TIMEZONE=$TIMEZONE" >> deploy.conf
 
 #Update
 sudo /usr/bin/apt-get update && sudo /usr/bin/apt-get full-upgrade -y
